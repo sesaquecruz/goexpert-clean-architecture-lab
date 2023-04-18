41f616842ae8d31a374d0625f803887177f116a3
Sesaque Cruz <cruz.sesaque@gmail.com>
2023-04-18 00:08:28 -0300

    Update order repository

--- internal/infra/database/order_repository.go
+++ internal/infra/database/order_repository.go
@@ -17,32 +17,32 @@ func NewOrderRepository(db *sql.DB) *OrderRepository {
 	}
 }
 
-func (r *OrderRepository) Save(ctx context.Context, order entity.Order) error {
+func (r *OrderRepository) Save(ctx context.Context, order *entity.Order) error {
 	stmt, err := r.Db.Prepare("INSERT INTO orders (id, price, tax, final_price) VALUES (?, ?, ?, ?)")
 	if err != nil {
 		return err
 	}
-	stmt.Close()
+	defer stmt.Close()
 
 	_, err = stmt.ExecContext(ctx, order.Id, order.Price, order.Tax, order.FinalPrice)
 	return err
 }
 
-func (r *OrderRepository) FindAll(ctx context.Context) ([]entity.Order, error) {
+func (r *OrderRepository) FindAll(ctx context.Context) ([]*entity.Order, error) {
 	rows, err := r.Db.QueryContext(ctx, "SELECT id, price, tax, final_price FROM orders")
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
 
-	var orders []entity.Order
+	var orders []*entity.Order
 	for rows.Next() {
 		var order entity.Order
 		if err := rows.Scan(&order.Id, &order.Price, &order.Tax, &order.FinalPrice); err != nil {
 			return nil, err
 		}
 
-		orders = append(orders, order)
+		orders = append(orders, &order)
 	}
 
 	return orders, nil
