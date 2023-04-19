130ed5c1235057f471af0c97052cd9e07cf3fea3
Sesaque Cruz <cruz.sesaque@gmail.com>
2023-04-19 16:51:03 -0300

    Add examples of orders

---
+++ migrations/sql/0002.sql
@@ -0,0 +1,9 @@
+USE `store` ;
+
+INSERT INTO orders (id, price, tax, final_price) VALUES ('7d94ce37-e7bb-4ef6-9cf4-4730ee69baeb', 10.0, 1.0, 11.0);
+
+INSERT INTO orders (id, price, tax, final_price) VALUES ('dfcd6baf-a001-40c5-b5e6-c63e3db60e7f', 20.0, 2.0, 22.0);
+
+INSERT INTO orders (id, price, tax, final_price) VALUES ('c2f9d9e5-274f-4f68-8c72-3ec273fe5f09', 30.0, 3.0, 33.0);
+
+INSERT INTO orders (id, price, tax, final_price) VALUES ('5d9d1ad4-0cf7-428f-adc0-95e8931b5eea', 40.0, 4.0, 44.0);
