72fdb7ebc2e1cbc3a74fee48581cc4586b51710c
Sesaque Cruz <cruz.sesaque@gmail.com>
2023-04-18 23:16:58 -0300

    Update config file

--- config/config.go
+++ config/config.go
@@ -1,37 +1,44 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"fmt"
+	"os"
+	"reflect"
+)
 
-type config struct {
-	DBDriver          string `mapstructure:"DB_DRIVER"`
-	DBHost            string `mapstructure:"DB_HOST"`
-	DBPort            string `mapstructure:"DB_PORT"`
-	DBUser            string `mapstructure:"DB_USER"`
-	DBPassword        string `mapstructure:"DB_PASSWORD"`
-	DBName            string `mapstructure:"DB_NAME"`
-	AMQPHost          string `mapstructure:"AMQP_HOST"`
-	AMQPPort          string `mapstructure:"AMQP_PORT"`
-	AMQPUser          string `mapstructure:"AMQP_USER"`
-	AMQPPassword      string `mapstructure:"AMQP_PASSWORD"`
-	GRPCServerPort    string `mapstructure:"GRPC_SERVER_PORT"`
-	RESTServerPort    string `mapstructure:"REST_SERVER_PORT"`
-	GRAPHQLServerPort string `mapstructure:"GRAPHQL_SERVER_PORT"`
+type Config struct {
+	DBDriver          string `env:"DB_DRIVER"`
+	DBHost            string `env:"DB_HOST"`
+	DBPort            string `env:"DB_PORT"`
+	DBUser            string `env:"DB_USER"`
+	DBPassword        string `env:"DB_PASSWORD"`
+	DBName            string `env:"DB_NAME"`
+	AMQPHost          string `env:"AMQP_HOST"`
+	AMQPPort          string `env:"AMQP_PORT"`
+	AMQPUser          string `env:"AMQP_USER"`
+	AMQPPassword      string `env:"AMQP_PASSWORD"`
+	GRPCServerPort    string `env:"GRPC_SERVER_PORT"`
+	RESTServerPort    string `env:"REST_SERVER_PORT"`
+	GRAPHQLServerPort string `env:"GRAPHQL_SERVER_PORT"`
 }
 
-func LoadConfig(path string) (*config, error) {
-	viper.AddConfigPath(path)
-	viper.SetConfigName("config")
-	viper.SetConfigType("env")
-	viper.SetConfigFile(".env")
-	viper.AutomaticEnv()
-	if err := viper.ReadInConfig(); err != nil {
-		panic(err)
+func LoadConfig() (*Config, error) {
+	config := Config{}
+
+	valueOf := reflect.ValueOf(&config).Elem()
+	typeOf := valueOf.Type()
+
+	for i := 0; i < valueOf.NumField(); i++ {
+		field := valueOf.Field(i)
+		envName := typeOf.Field(i).Tag.Get("env")
+		envValue, ok := os.LookupEnv(envName)
+
+		if !ok {
+			return nil, fmt.Errorf("%s was not found", envName)
+		}
+
+		field.SetString(envValue)
 	}
 
-	var cfg *config
-	if err := viper.Unmarshal(&cfg); err != nil {
-		panic(err)
-	}
-
-	return cfg, nil
+	return &config, nil
 }
